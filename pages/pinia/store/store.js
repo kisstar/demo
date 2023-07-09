3844ec42591fd875a7254a91106468d92374c33f
kisstar <dwh.chn@foxmail.com>
2023-07-10 00:02:24 +0800

    feat(pinia): 实现 defineStore() 方法

--- pages/pinia/store/store.js
+++ pages/pinia/store/store.js
@@ -1,1 +1,112 @@
-export function defineStore() {}
+import {
+  getCurrentInstance,
+  inject,
+  reactive,
+  effectScope,
+  computed,
+  isRef,
+  isReactive,
+} from './vue.js';
+import { piniaSymbol } from './rootStore.js';
+
+function isComputed(v) {
+  return isRef(v) && v.effect;
+}
+
+function createSetupStore(id, setup, pinia, isOptions) {
+  const initialState = pinia.state.value[id];
+  const store = reactive({}); // 每个 store 就是一个响应式对象
+  let scope;
+
+  if (!initialState && !isOptions) {
+    pinia.state.value[id] = {};
+  }
+
+  const setupStore = pinia._e.run(() => {
+    scope = effectScope();
+
+    return scope.run(() => setup());
+  });
+
+  function wrapAction(name, fun) {
+    return function (...args) {
+      return fun.call(store, ...args);
+    };
+  }
+
+  for (let key in setupStore) {
+    const value = setupStore[key];
+
+    if (typeof value === 'function') {
+      setupStore[key] = wrapAction(key, value);
+    }
+    if ((isRef(value) && !isComputed(value)) || isReactive(value)) {
+      if (!isOptions) {
+        pinia.state.value[id][key] = value;
+      }
+    }
+  }
+
+  pinia._s.set(id, store);
+  Object.assign(store, setupStore);
+
+  return store;
+}
+
+function createOptionsStore(id, options, pinia) {
+  const { state, getters, actions } = options;
+
+  function setup() {
+    const localState = (pinia.state.value[id] = state ? state() : {});
+
+    return Object.assign(
+      localState,
+      actions,
+      Object.keys(getters || {}).reduce((memo, name) => {
+        memo[name] = computed(() => {
+          const store = pinia._s.get(id);
+
+          return getters[name].call(store);
+        });
+
+        return memo;
+      }, {})
+    );
+  }
+
+  return createSetupStore(id, setup, pinia, true);
+}
+
+export function defineStore(idOrOptions, setup) {
+  const isSetupStore = typeof setup === 'function';
+  let id, options;
+
+  if (typeof idOrOptions === 'string') {
+    id = idOrOptions;
+    options = setup;
+  } else {
+    id = idOrOptions.id;
+    options = idOrOptions;
+  }
+
+  function useStore() {
+    // 根据 Vue 模块提供的方法获取当前的应用
+    const app = getCurrentInstance();
+    // 如果存在说明是在 Vue 组件中在使用，此时可以通过 Vue 提供的 inject() API 获取之前注入的 pinia 对象
+    const pinia = app && inject(piniaSymbol);
+
+    if (!pinia._s.has(id)) {
+      if (isSetupStore) {
+        createSetupStore(id, setup, pinia);
+      } else {
+        createOptionsStore(id, options, pinia);
+      }
+    }
+
+    const store = pinia._s.get(id);
+
+    return store;
+  }
+
+  return useStore;
+}
