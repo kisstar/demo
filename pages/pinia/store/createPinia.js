70372744289e8140d1398293496dfbf77e3af563
kisstar <dwh.chn@foxmail.com>
2023-07-09 14:45:34 +0800

    feat(pinia): 实现 createPinia() 方法

---
+++ pages/pinia/store/createPinia.js
@@ -0,0 +1,24 @@
+import {
+  ref,
+  effectScope,
+} from 'https://cdn.bootcdn.net/ajax/libs/vue/3.2.47/vue.esm-browser.js';
+
+const piniaSymbol = Symbol();
+
+export function createPinia() {
+  const scope = effectScope();
+  const state = scope.run(() => ref({}));
+  const pinia = {
+    _e: scope,
+    _s: new Map(), // 存放所有的 store 信息
+    install(app) {
+      // 让所有的 store 都可以获取到 pinia 对象，然后就可以存储到 Map 中
+      app.provide(piniaSymbol, pinia);
+      // 让所有的组件也可以访问
+      app.config.globalProperties.$pinia = pinia;
+    },
+    state,
+  };
+
+  return pinia;
+}
