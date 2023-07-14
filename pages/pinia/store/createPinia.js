2a6412d659ae0152b68cd32176cd8f4c3ae0bebd
kisstar <dwh.chn@foxmail.com>
2023-07-14 22:28:04 +0800

    feat(pinia): 支持组件外使用

--- pages/pinia/store/createPinia.js
+++ pages/pinia/store/createPinia.js
@@ -1,6 +1,16 @@
 import { ref, effectScope } from './vue.js';
 import { piniaSymbol } from './rootStore.js';
 
+let activePinia;
+
+export function setActivePinia(pinia) {
+  activePinia = pinia;
+}
+
+export function getActivePinia() {
+  return activePinia;
+}
+
 export function createPinia() {
   const scope = effectScope();
   const state = scope.run(() => ref({}));
@@ -10,6 +20,7 @@ export function createPinia() {
     _s: new Map(), // 存放所有的 store 信息
     _p,
     install(app) {
+      setActivePinia(pinia);
       // 让所有的 store 都可以获取到 pinia 对象，然后就可以存储到 Map 中
       app.provide(piniaSymbol, pinia);
       // 让所有的组件也可以访问
