01f4941a0d0bdc43b36f3b79d85fe58029da7743
kisstar <dwh.chn@foxmail.com>
2023-07-15 14:27:11 +0800

    feat(vue): 创建 createRenderer 的结构

--- pages/vue/runtime-dom/index.js
+++ pages/vue/runtime-dom/index.js
@@ -1,6 +1,22 @@
 import { nodeOps } from './nodeOps.js';
 import { patchProp } from './patchProp.js';
+import { createRenderer } from '../runtime-core/index.js';
 
 const rendererOptions = Object.assign({}, nodeOps, { patchProp });
 
-export { rendererOptions };
+export function createApp(rootComp, props) {
+  const app = createRenderer(rendererOptions).createApp(rootComp, props);
+  const { mount } = app;
+
+  app.mount = function (selector) {
+    const container = nodeOps.querySelector(selector);
+
+    // 清空容器
+    container.innerHTML = '';
+
+    // 将组件渲染成 DOM 挂载容器中
+    mount(container);
+  };
+
+  return app;
+}
