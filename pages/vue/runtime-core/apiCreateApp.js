02e0f99e1f29759d942be029cd4e43b490dad955
kisstar <dwh.chn@foxmail.com>
2023-07-15 15:00:55 +0800

    feat(vue): 创建虚拟节点

--- pages/vue/runtime-core/apiCreateApp.js
+++ pages/vue/runtime-core/apiCreateApp.js
@@ -1,11 +1,19 @@
+import { createVNode } from './vnode.js';
+
 export function careateAppAPI(render) {
   return function createApp(rootComp, props) {
-    const app = {};
+    const app = {
+      __props: props,
+      __component: rootComp,
+      __container: null,
+    };
 
     // 根据组件创建虚拟 DOM
     // 将得到的虚拟 DOM 和 容器交给 render() 函数进行渲染
     app.mount = function (container) {
-      let vnode = {};
+      app.__container = container;
+
+      const vnode = createVNode(rootComp, props);
 
       render(vnode, container);
     };
---
+++ pages/vue/shared/index.js
@@ -0,0 +1,14 @@
+export const isObject = (value) => typeof value === 'object' && value !== null;
+
+export const isArray = Array.isArray;
+
+export const isIntergerKey = (value) => parseInt(value) + '' === value;
+
+export const hasOwn = (target, key) =>
+  Object.prototype.hasOwnProperty.call(target, key);
+
+export const isFunction = (value) => typeof value === 'function';
+
+export const isString = (value) => typeof value === 'string';
+
+export * from './shapeFlag.js';
