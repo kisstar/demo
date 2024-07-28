df95c1b8be0bc8a5bd90db3b23d0803352a608aa
Kisstar <dwh.chn@foxmail.com>
2024-07-28 16:52:45 +0800

    refactor: 调整代码组织结构

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+work_dir=$(pwd)
+vue_app_dir="packages/vue"
+
+npm i -g pnpm
+
+# 安装编译 Vue 应用
+cd $vue_app_dir
+pnpm i
+npm run build
+
+# 复制静态资源
+cd $work_dir
+cp -r packages/vue/dist dist
+cp -r pages/ dist/
