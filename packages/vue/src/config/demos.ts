883824e0a896878318d12f023487ae6abbbdb4e2
Kisstar <dwh.chn@foxmail.com>
2024-10-04 20:00:58 +0800

    feat: base public path

--- packages/vue/src/config/demos.ts
+++ packages/vue/src/config/demos.ts
@@ -1,30 +1,32 @@
+import { BASE_DIR } from "./url";
+
 export const ALL_DEMOS = [
 	{
 		name: "CSS",
 		children: [
 			{
 				name: "【视差滚动】通过固定背景实现视差滚动",
-				url: "/parallax/bg.html",
+				url: `${BASE_DIR}parallax/bg.html`,
 			},
 			{
 				name: "【视差滚动】通过监听 Scroll 事件实现视差滚动",
-				url: "/parallax/event.html",
+				url: `${BASE_DIR}parallax/event.html`,
 			},
 			{
 				name: "【视差滚动】通过固定定位实现视差滚动",
-				url: "/parallax/fixed.html",
+				url: `${BASE_DIR}parallax/fixed.html`,
 			},
 			{
 				name: "【视差滚动】通过 3D 转换实现视差滚动",
-				url: "/parallax/transform.html",
+				url: `${BASE_DIR}parallax/transform.html`,
 			},
 			{
 				name: "【Flex 多列布局】通过 Flex 实现多列布局，并使用负外边距（margin）解决首列多余的边距",
-				url: "/flex/margin-list.html",
+				url: `${BASE_DIR}flex/margin-list.html`,
 			},
 			{
 				name: "【Flex 多列布局】通过 Flex 实现多列布局，并使用 gap 设置边距",
-				url: "/flex/gap-list.html",
+				url: `${BASE_DIR}flex/gap-list.html`,
 			},
 		],
 	},
